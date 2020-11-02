491144099921584a35ef90a57ea79de091e322b0
Nadine Coelho <nadinecoelho9@gmail.com>
2020-11-02 15:07:05 -0300

    add css-loader and style-loader to config

--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,14 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      //array the contains different object
+      //each loader is an object the contains different things
+      {
+        test: /\.css$/, //test particular files, don't every single file. Its gonna test only files finished with the css extension.
+        use: ["style-loader", "css-loader"], //with loaader we'll gonna use it (css loader takes your css and it turns in to javascript, style loader will take that javascript and inject it into the DOM (they load in reverse order, even if "style-loader" comes first))
+      },
+    ],
+  },
 };
