035521188c508b0dd61c4bb8bca43d341d51bf3b
Nadine Coelho <nadinecoelho9@gmail.com>
2020-11-02 17:59:33 -0300

    add sass-loader to config

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,12 @@ module.exports = {
       //array the contains different object
       //each loader is an object the contains different things
       {
-        test: /\.css$/, //test particular files, don't every single file. Its gonna test only files finished with the css extension.
-        use: ["style-loader", "css-loader"], //with loaader we'll gonna use it (css loader takes your css and it turns in to javascript, style loader will take that javascript and inject it into the DOM (they load in reverse order, even if "style-loader" comes first))
+        test: /\.scss$/, //test particular files, don't every single file. Its gonna test only files finished with the css extension.
+        use: [
+          "style-loader", // 3. Inject styles into DOM
+          "css-loader", // 2.  Turns css into common js
+          "sass-loader", // 1. Turns sass into css
+        ],
       },
     ],
   },
