17aca5c57822d86cf0274e71b7f7dfb8bd29184c
Nadine Coelho <nadinecoelho9@gmail.com>
2020-11-05 21:21:38 -0300

    Minify HTML, CSS and JS in production

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 module.exports = {
   // definy entry point
@@ -7,23 +7,11 @@ module.exports = {
     main: "./src/index.js",
     vendor: "./src/vendor.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
+
   module: {
     rules: [
       //array the contains different object
       //each loader is an object the contains different things
-      {
-        test: /\.scss$/, //test particular files, don't every single file. Its gonna test only files finished with the css extension.
-        use: [
-          "style-loader", // 3. Inject styles into DOM
-          "css-loader", // 2.  Turns css into common js
-          "sass-loader", // 1. Turns sass into css
-        ],
-      },
       {
         test: /\.html$/,
         use: ["html-loader"], // require any images and imported into Javascript
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,10 @@ const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // will delete the dist directory every time we build
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -11,5 +15,36 @@ module.exports = merge(common, {
     filename: "[name].[contenthash].bundle.js", // [contenthash] Convert an input of any length in to a fixed size string of text. no matter how long the text is, it will be converted into an array of numbers an letters.
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new CleanWebpackPlugin()],
+  optimization: {
+    minimizer: [
+      // its an array cause because sometimes we're minifying more of one extension.
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/, //test particular files, don't every single file. Its gonna test only files finished with the css extension.
+        use: [
+          MiniCssExtractPlugin.loader, //. Extract css into files
+          "css-loader", // 2.  Turns css into common js
+          "sass-loader", // 1. Turns sass into css
+        ],
+      },
+    ],
+  },
 });
